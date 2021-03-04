d8a12c61880425896b723f4cc1ae76e1d591e476
Gabriel N. Parente <gnp32@vi.cs.rutgers.edu>
2021-03-03 21:25:17 -0500

    Added function to Ignore Certain file names (Part 2), started function to create the new name and Open it

--- ww.c
+++ ww.c
@@ -7,6 +7,37 @@
 #include <unistd.h>
 #include "ww.h"
 
+#define DT_REG 8
+
+//determines if the file in the directory starts with '.' or "wrap."
+//if it does we DO NOT word wrap it
+//TODO: Test this more incase there is bugs with varying file name sizes
+int ignoreFileName(char name[]){
+
+    if (name[0] == '.' || strncmp("wrap.", name, strlen("wrap.")) == 0){
+        return 0;
+    }
+    return 1;
+}
+//TODO: Finish and clean up this function
+char* createFileName(char fileName[], char* path){
+    //create the new file name with the "wrap." prefix
+    char* prefix = "wrap.";
+    
+    int prefixLength = strlen(prefix);
+    int fileNameLength = strlen(fileName);
+    int pathFileLength = strlen(path);
+    
+    int newFileLength = prefixLength + fileNameLength + pathFileLength;
+    
+    char* newFileName = malloc((newFileLength * sizeof(char)));
+    memcpy(newFileName, prefix, prefixLength);
+    memcpy(newFileName+prefixLength, fileName, fileNameLength);
+    memcpy(newFileName+prefixLength+fileNameLength, path, pathFileLength);
+
+    return newFileName;
+}
+
 int isDirectory(char *name){
     struct stat data;
 
@@ -29,11 +60,11 @@ int wrap(char * filename, int width){
 
     //TODO: figure out why bytes_read doesn't work.
     //TODO: actually format the file properly
-    int bytes_read=0;
+    int bytes_read = 0;
     char* buf = malloc(sizeof(char)*width);
     int f = open(filename, O_RDONLY);
     //if you can't open the file, return failure
-    if(f==-1)
+    if(f == -1)
         return 1;
 
     bytes_read = read(f,&buf,width);
@@ -46,32 +77,35 @@ int wrap(char * filename, int width){
 int main(int argc, char* argv[]){
 
     //Check if proper arguments are given. If it is not 3 (ex: [./ww 80]), then return with EXIT_FAILURE.
-    if(argc!=3)
+    if(argc != 3)
         return EXIT_FAILURE;
 
     //if argv is a directory, word wrap all the files in it
-    //TODO: change argv[1] -> argv[2] [FIXED by broteam/iman]
     //TODO: check if this works with paths
    if (isDirectory(argv[2])) {
-       puts("dir");
-       //Now we find all the files besides "."/".."
        DIR *dirp = (opendir(argv[2]));
        struct dirent *de;
        //TODO: replace d_type with the use of stat()
-       //test line
         while ((de = readdir(dirp)) != NULL) {
-            if (de->d_type == DT_REG) {
-                printf("%s\n", de->d_name);
+            //We check if the file is a regular file, and does not start with "." or "wrap."
+            if (de->d_type == DT_REG && ignoreFileName(de->d_name)) {
+              // char* newFileName = createFileName(de->d_name, argv[2]);
+              // free(newFileName);
+               // char pathFile[100];
+               // sprintf(pathFile, "%s/wraptest", argv[2]);
+               // int fd = open(pathFile, O_CREAT);
+               printf("%s\n", createFileName(de->d_name, argv[2]));
+               
            }
        }
     
        closedir(dirp);
    }
-
+/*
    //get width  
    int width = atoi(argv[1]);
    //wrap with file name and width
    wrap(argv[2],width); 
-   
+*/ 
    return EXIT_SUCCESS;
 }
