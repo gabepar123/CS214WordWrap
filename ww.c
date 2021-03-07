6a943834ff673fbc24ba5bb2578311e72613c520
Iman I. Din <iid5@rm.cs.rutgers.edu>
2021-03-06 20:07:10 -0500

    worked on wrap(), got one version of it that i'm satisfied with for now!

--- ww.c
+++ ww.c
@@ -63,49 +63,73 @@ int isDirectory(char *name){
 int wrap(int fin, int fout, unsigned width){
 
     int bytes_read;
-    char* buf = malloc(sizeof(char)*(width/2));
-    bytes_read = read(fin,&buf,(width/2));
+    strbuf_t* buf;
+    strbuf_t* temp;
+    sb_init(buf,width/2);
+    sb_init(temp,width/2);
+    bytes_read = read(fin,&buf->data,(width/2));
 
     //TODO: make this algo run throughout the whole file, and not the first (width/2) bytes.
     int i;
     int started=0;
     int startIndex=0;
-    int endIndex=0;
+    int wordBytes=0;
     int totalBytes=0;
-    //while(fin!=EOF){
-        for(i=0;i<width/2;i++){
+    while(bytes_read!=0){
+        for(i=0;i<width/2;++i){
             if(totalBytes>=width){
                 //TODO: write in a new line after every x bytes
+                write(fout,"\n",1);
                 //if the amount we read is equal/greater than the amount we want to wrap, then break
                 break;
             }
             //if it is a space, then we know the word has ended
-            if(isspace(buf[i])){
-                //set the last index of the word
-                endIndex=i-1;
+            if(isspace(buf->data[i])){
                 //write it to the output file
-                write(fout, &buf[startIndex], (startIndex+endIndex)-1);
+                write(fout, &buf->data[startIndex], wordBytes);
                 //say that we did not start at a word
                 started=0;
+                //clear about of wordBytes
+                wordBytes=0;
             }
-            else{
-                if(started==0){
+            else{//if not a space, check if we are in the middle of a word
+                if(started==0){//not in the middle of a word
                     startIndex=i;
+                    wordBytes=1;
                     started=1;
+                }
+                else{//in middle of word
+                    wordBytes++;
                 }
             }
             totalBytes++;
-        }
+        }//end of buffer reading
         //if we are in a word right now but the buffer has ended, stash it
         //TODO: stash the end of a buffer properly
+        //TODO: re-read bytes (outside if statement)
         if(started==1){
-
+            int j;
+            int x = startIndex;
+            //temp is the rest of the word in the buffer
+            for(j=0;j<wordBytes;++j){
+                sb_append(temp,buf->data[x]);
+                ++x;
+            }
         }
-    //}
-   
-    //write(fout, &buf[startIndex], (startIndex+endIndex)-1);
+        sb_destroy(buf);
+        sb_init(buf,width/2);
+        bytes_read = read(fin,&buf->data,(width/2));
+        int k;
+        for(k=0;k<wordBytes;++k){
+            sb_append(buf,temp->data[k]);
+        }
+        sb_destroy(temp);
+        sb_init(temp,width/2);
 
-    free(buf);
+    }//end of file reading
+
+    sb_destroy(buf);
+    sb_destroy(temp);
     return 0;
 }
 
@@ -114,6 +138,9 @@ int main(int argc, char* argv[]){
     //Check if proper arguments are given. If it is not 3 (ex: [./ww 80]), then return with EXIT_FAILURE.
     if(argc != 3)
         return EXIT_FAILURE;
+
+    //get width  
+    unsigned width = atoi(argv[1]);
 
     //if argv is a directory, word wrap all the files in it
    if (isDirectory(argv[2])) {
@@ -127,16 +154,12 @@ int main(int argc, char* argv[]){
                 int fd = open(newFileName, O_CREAT, S_IRWXU);
                 free(newFileName);
                 //TODO: this is the point where we call wordwrap(fd,...,...)
+                wrap(,fd,width);
            }
        }
     
        closedir(dirp);
    }
-/*
-   //get width  
-   unsigned width = atoi(argv[1]);
-   //wrap with file name and width
-   wrap(argv[2],width); 
-*/ 
+
    return EXIT_SUCCESS;
 }
